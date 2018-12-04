6b2a58bc6f5ccbed76196952883f745e4af60111
DanielMayles <daniel.mayles@bossastudios.com>
2018-12-04 14:04:39 +0000

    Refactoring placing of stuff

--- Assets/Scripts/EditorModeScripts/EditorDialogueObjectPlacer.cs
+++ Assets/Scripts/EditorModeScripts/EditorDialogueObjectPlacer.cs
@@ -15,23 +15,53 @@ public class EditorDialogueObjectPlacer : MonoBehaviour {
 	private void CreateDialogueObject() {
 		_currentSelectedDialogueObject = Instantiate<DialogueObject>(_dialoguePrefab);
 	}
-	
-	void Update () {
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			if (_currentSelectedDialogueObject != null) 
+			{
+				PlaceDialogueObject();
+			} 
+			else 
+			{
+				GrabOrCreateDialogueObject();
+			}
+		} 
+		else if (Input.GetMouseButtonDown(1)) 
+		{
+
+		}
+		UpdateHeldDialogueObject();	
+	}
+
+	void PlaceDialogueObject() {
+		if (_currentSelectedDialogueObject != null) {
+			Shell.dialogueService.AddNewDialogueObject(_currentSelectedDialogueObject);
+			_currentSelectedDialogueObject = null;
+		}
+	}
+
+	void GrabOrCreateDialogueObject() {
+		RaycastHit raycastResult;
+		if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out raycastResult, 100, -1)) {
+			DialogueObject hitDialogueObject = raycastResult.collider.GetComponent<DialogueObject>();
+			if (hitDialogueObject != null) {
+				_currentSelectedDialogueObject = hitDialogueObject;
+			}
+			else {
+				CreateDialogueObject();
+			}
+		}
+	}
+
+	void UpdateHeldDialogueObject() {
 		if (_currentSelectedDialogueObject != null) {
 			RaycastHit raycastResult;
 			if (Physics.Raycast(_camera.transform.position, _camera.transform.forward, out raycastResult, 100, _targetLayers)) {
 				Vector3 hitpoint = raycastResult.point;
 				_currentSelectedDialogueObject.transform.position = new Vector3(Mathf.Round(hitpoint.x), 0, Mathf.Round(hitpoint.z));
 			}
-
-			if (Input.GetMouseButtonDown(0) && _currentSelectedDialogueObject != null) {
-				Shell.dialogueService.AddNewDialogueObject(_currentSelectedDialogueObject);
-				_currentSelectedDialogueObject = null;
-			}
 		}
-		else if(Input.GetMouseButtonDown(0)) {
-			CreateDialogueObject();
-		}
-
 	}
 }
